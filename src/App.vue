3a7c3e02951075bb01489fed8c6029b61712d9b1
sakura3 <shiguowei333@outlook.com>
2024-11-28 23:05:38 +0800

    配置element-plus组件国际化

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <div>
-    <h1>123</h1>
-  </div>
+  <el-button type="primary" :icon="Plus"></el-button>
+  <el-pagination layout="prev, pager, next" :total="50">
+  </el-pagination>
 </template>
 
 <script setup lang="ts">
-
+  import {Plus} from '@element-plus/icons-vue'
 </script>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+// 引入element-plus插件
+import ElementPlus from 'element-plus'
+import 'Element-plus/dist/index.css'
+// 配置element-plus国际化
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+// 获取应用实例对象
+const app = createApp(App)
+// 安装element-plus插件
+app.use(ElementPlus,{
+  locale:zhCn // 配置element-plus国际化
+})
+// 将应用挂载到挂载点上
+app.mount('#app')
 
-createApp(App).mount('#app')
