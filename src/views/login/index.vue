31184c263dc528139d5929e73adfbe43ff126417
shi_guowei <shiguowei333@outlook.com>
2024-12-09 18:06:13 +0800

    创建图片文件静态资源

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,13 +1,18 @@
 <template>
-    <div>
-      <h1>我是登录页</h1>
+    <div class="login_container">
+      
     </div>
 </template>
   
 <script setup lang='ts'>
-  
+  import test from '../../assets/images/background.jpg'
 </script>
   
-<style>
-  
+<style scoped lang="scss">
+  .login_container {
+    width: 100%;
+    height: 100vh;
+    background-color: url('@/assets/images/background.jpg');
+    background-size: cover;
+  }
 </style>
