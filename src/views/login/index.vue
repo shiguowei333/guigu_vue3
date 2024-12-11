6e0626b0e7c73258e7189a21aabb09d9d34a60a7
sakura3 <shiguowei333@outlook.com>
2024-12-11 22:34:28 +0800

    登录表单自定义规则校验功能

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,13 +3,13 @@
       <el-row>
           <el-col :span="12" :xs="0"></el-col>
           <el-col :span="12" :xs="24">
-            <el-form class="login_form">
+            <el-form class="login_form" :model="loginForm" :rules="rules" ref="loginForms">
                 <h1>hello</h1>
                 <h2>欢迎来到硅谷甄选</h2>
-                <el-form-item label="">
+                <el-form-item prop="username">
                   <el-input :prefix-icon="User" v-model="loginForm.username"></el-input>
                 </el-form-item>
-                <el-form-item>
+                <el-form-item prop="password">
                   <el-input type="password" :prefix-icon="Lock" v-model="loginForm.password" show-password></el-input>
                 </el-form-item>
                 <el-form-item>
@@ -30,26 +30,64 @@
     let loading = ref(false)
     let $router = useRouter()
     let useStore = useUserStore()
+    let loginForms = ref()
     // 收集账号与密码数据
     let loginForm = reactive({username: 'admin',password: '123456'})
+    // 自定义校验姓名函数
+    const validatorUserName = (rules: any,value: any,callback: any) => {
+      if(value.length >= 5) {
+        callback()
+      }else {
+        callback(new Error('账号长度至少5位'))
+      }
+    }
+    // 自定义校验密码函数
+    const validatorPassword = (rules: any,value: any,callback: any) => {
+      if(value.length >= 6) {
+        callback()
+      }else {
+        callback(new Error('密码长度至少6位'))
+      }
+    }
+    // 表单校验规则
+    const rules = {
+      username: [
+        {
+          trigger: 'change',
+          validator: validatorUserName
+        }
+      ],
+      password: [
+        {
+          trigger: 'change',
+          validator: validatorPassword
+        }
+      ]
+    }
+    
     // 登录按钮回调
     const login = async() => {
-      loading.value = true
       try {
-        await useStore.userLogin(loginForm)
-        $router.push('/')
-        ElNotification({
-          type: 'success',
-          message: '欢迎回来',
-          title: `HI,${getTime()}好`
-        })
+        await loginForms.value.validate()
         loading.value = true
+        try {
+          await useStore.userLogin(loginForm)
+          $router.push('/')
+          ElNotification({
+            type: 'success',
+            message: '欢迎回来',
+            title: `HI,${getTime()}好`
+          })
+          loading.value = true
+        } catch (error) {
+          loading.value = false
+          ElNotification({
+            type: 'error',
+            message: (error as Error).message
+          })
+        }
       } catch (error) {
-        loading.value = false
-        ElNotification({
-          type: 'error',
-          message: (error as Error).message
-        })
+        
       }
     }
 </script>
