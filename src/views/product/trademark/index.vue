80ec82ba0d841717cd86c164109455cf824292d2
sakura3 <shiguowei333@outlook.com>
2024-12-17 20:00:48 +0800

    关闭dialog窗口清空表单数据

--- src/views/product/trademark/index.vue
+++ src/views/product/trademark/index.vue
@@ -1,62 +1,64 @@
 <template>
-  <el-card :body-style="{height:'90vh'}">
-    <el-button type="primary" icon="Plus" @click="showDialog">添加品牌</el-button>
-    <el-table style="margin: 30px 0;height: 72vh;" border :data="trademarkArr" table-layout="auto" :row-style="{height: '6.5vh'}">
-      <el-table-column align="center" label="序号" type="index"></el-table-column>
-      <el-table-column align="center" label="品牌名称" prop="tmName"></el-table-column>
-      <el-table-column align="center" label="品牌LOGO">
-        <template #="{row}">
-          <img :src="row.logoUrl" style="width: 30px;height: 30px;">
-        </template>
-      </el-table-column>
-      <el-table-column align="center" label="创建时间" prop="createTime"></el-table-column>
-      <el-table-column align="center" label="修改时间" prop="updateTime"></el-table-column>
-      <el-table-column align="center" label="操作">
-        <template #="">
-          <el-button type="primary" icon="Edit" @click="showDialog"></el-button>
-          <el-button type="primary" icon="Delete"></el-button>
-        </template>
-      </el-table-column>
-    </el-table>
-    <el-pagination
-     v-model:current-page="pageNo"
-     v-model:page-size="pageSize"
-     :page-sizes="[10,20,50,100]"
-     :background="true" layout="->, total, sizes, prev, pager, next, jumper"
-     :total="total"
-     @change="getHasTrademark"
+  <div>
+    <el-card :body-style="{height:'90vh'}">
+      <el-button type="primary" icon="Plus" @click="showDialog">添加品牌</el-button>
+      <el-table style="margin: 30px 0;height: 72vh;" border :data="trademarkArr" table-layout="auto" :row-style="{height: '6.5vh'}">
+        <el-table-column align="center" label="序号" type="index"></el-table-column>
+        <el-table-column align="center" label="品牌名称" prop="tmName"></el-table-column>
+        <el-table-column align="center" label="品牌LOGO">
+          <template #="{row}">
+            <img :src="row.logoUrl" style="width: 30px;height: 30px;">
+          </template>
+        </el-table-column>
+        <el-table-column align="center" label="创建时间" prop="createTime"></el-table-column>
+        <el-table-column align="center" label="修改时间" prop="updateTime"></el-table-column>
+        <el-table-column align="center" label="操作">
+          <template #="">
+            <el-button type="primary" icon="Edit" @click="showDialog"></el-button>
+            <el-button type="primary" icon="Delete"></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <el-pagination
+       v-model:current-page="pageNo"
+       v-model:page-size="pageSize"
+       :page-sizes="[10,20,50,100]"
+       :background="true" layout="->, total, sizes, prev, pager, next, jumper"
+       :total="total"
+       @change="getHasTrademark"
+      >
+      </el-pagination>
+    </el-card>
+    <el-dialog
+      v-model="dialogVisible"
+      title="新增品牌"
+      width="600"
+      @close="emptyForm"
     >
-    </el-pagination>
-  </el-card>
-  <el-dialog
-    v-model="dialogVisible"
-    title="新增品牌"
-    width="600"
-    :close="emptyDialog"
-  >
-    <el-form :style="{width:'80%'}"  label-position="left" ref="traForm">
-      <el-form-item label="品牌名称" label-width="80px">
-        <el-input aria-placeholder="请输入品牌名称" v-model="trademarkParams.tmName"></el-input>
-      </el-form-item>
-      <el-form-item label="品牌logo" label-width="80px">
-        <el-upload
-          class="avatar-uploader"
-          action="/api/admin/product/fileUpload"
-          :show-file-list="false"
-          :before-upload="beforeAvatarUpload"
-          :on-success="handleAvatarSuccess"
-          accept=".jpg,.png,.gif,.jpeg"
-        >
-          <img v-if="trademarkParams.logoUrl" :src="trademarkParams.logoUrl" class="avatar" />
-          <el-icon v-else class="avatar-uploader-icon"><Plus /></el-icon>
-        </el-upload>
-      </el-form-item>
-    </el-form>
-    <template #footer>
-      <el-button @click="closeDialog">取消</el-button>
-      <el-button type="primary" @click="confirm">确定</el-button>
-    </template>
-  </el-dialog>
+      <el-form :style="{width:'80%'}"  label-position="left">
+        <el-form-item label="品牌名称" label-width="80px">
+          <el-input aria-placeholder="请输入品牌名称" v-model="trademarkParams.tmName"></el-input>
+        </el-form-item>
+        <el-form-item label="品牌logo" label-width="80px">
+          <el-upload
+            class="avatar-uploader"
+            action="/api/admin/product/fileUpload"
+            :show-file-list="false"
+            :before-upload="beforeAvatarUpload"
+            :on-success="handleAvatarSuccess"
+            accept=".jpg,.png,.gif,.jpeg"
+          >
+            <img v-if="trademarkParams.logoUrl" :src="trademarkParams.logoUrl" class="avatar" />
+            <el-icon v-else class="avatar-uploader-icon"><Plus /></el-icon>
+          </el-upload>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <el-button @click="closeDialog">取消</el-button>
+        <el-button type="primary" @click="confirm">确定</el-button>
+      </template>
+    </el-dialog>
+  </div>
 </template>
   
 <script setup lang='ts'>
@@ -66,7 +68,6 @@
   import { TradeMark } from '@/api/product/trademark/type'
   import { ElMessage } from 'element-plus'
   import type { UploadProps } from 'element-plus'
-  let traForm = ref()
   let pageNo = ref<number>(1)
   let pageSize = ref<number>(10)
   let total = ref<number>(0)
@@ -97,7 +98,8 @@
   }
 
   const beforeAvatarUpload: UploadProps['beforeUpload'] = (rawFile) => {
-    if(rawFile.type=='image/png'||rawFile.type=='image/jpg'||rawFile.type=='image/gif') {
+    console.log(rawFile.type)
+    if(rawFile.type=='image/png'||rawFile.type=='image/jpg'||rawFile.type=='image/gif'||rawFile.type=='image/jpeg') {
       if(rawFile.size/1024/1024<4) {
         return true
       }else {
@@ -110,7 +112,7 @@
     }else {
       ElMessage({
         type: 'error',
-        message: '上传文件格式务必PNG|JPG|GIF'
+        message: '上传文件格式务必PNG|JPG|JPEG'
       })
       return false
     }
@@ -134,8 +136,9 @@
       })
     }
   }
-  const emptyDialog = () => {
-    traForm.value.resetFields()
+  const emptyForm = () => {
+    trademarkParams.tmName = ''
+    trademarkParams.logoUrl = ''
   }
 </script>
 
