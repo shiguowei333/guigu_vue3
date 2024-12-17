4889a83eb7b1f250a8095872f015da209146e6ec
shi_guowei <shiguowei333@outlook.com>
2024-12-17 13:51:10 +0800

    对话框dialog静态搭建

--- src/views/product/trademark/index.vue
+++ src/views/product/trademark/index.vue
@@ -1,6 +1,6 @@
 <template>
   <el-card :body-style="{height:'90vh'}">
-    <el-button type="primary" icon="Plus">添加品牌</el-button>
+    <el-button type="primary" icon="Plus" @click="showDialog">添加品牌</el-button>
     <el-table style="margin: 30px 0;height: 72vh;" border :data="trademarkArr" table-layout="auto" :row-style="{height: '6.5vh'}">
       <el-table-column align="center" label="序号" type="index"></el-table-column>
       <el-table-column align="center" label="品牌名称" prop="tmName"></el-table-column>
@@ -13,7 +13,7 @@
       <el-table-column align="center" label="修改时间" prop="updateTime"></el-table-column>
       <el-table-column align="center" label="操作">
         <template #="">
-          <el-button type="primary" icon="Edit"></el-button>
+          <el-button type="primary" icon="Edit" @click="showDialog"></el-button>
           <el-button type="primary" icon="Delete"></el-button>
         </template>
       </el-table-column>
@@ -28,7 +28,27 @@
     >
     </el-pagination>
   </el-card>
-  
+  <el-dialog
+    v-model="dialogVisible"
+    title="新增品牌"
+    width="600"
+  >
+    <el-form :style="{width:'80%'}"  label-position="left">
+      <el-form-item label="品牌名称" label-width="80px">
+        <el-input aria-placeholder="请输入品牌名称"></el-input>
+      </el-form-item>
+      <el-form-item label="品牌logo" label-width="80px">
+        <el-upload
+          class="avatar-uploader"
+          action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
+          :show-file-list="false"
+        >
+          <img v-if="false" src="https://www.baidu.com" class="avatar" />
+          <el-icon v-else class="avatar-uploader-icon"><Plus /></el-icon>
+        </el-upload>
+      </el-form-item>
+    </el-form>
+  </el-dialog>
 </template>
   
 <script setup lang='ts'>
@@ -39,6 +59,7 @@
   let pageSize = ref<number>(10)
   let total = ref<number>(0)
   let trademarkArr = ref<Records>([])
+  let dialogVisible = ref<boolean>(false)
 
   onMounted(() => {
     getHasTrademark()
@@ -51,8 +72,42 @@
       trademarkArr.value = result.data.records
     }
   }
+
+  const showDialog = () => {
+    dialogVisible.value = true
+  }
+  const closeDialog = () => {
+    dialogVisible.value = false
+  }
 </script>
+
+<style scoped>
+.avatar-uploader .avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
+}
+</style>
+
+<style lang="scss">
+  .avatar-uploader .el-upload {
+  border: 1px dashed var(--el-border-color);
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+  transition: var(--el-transition-duration-fast);
+  }
   
-<style scoped lang="scss">
+  .avatar-uploader .el-upload:hover {
+    border-color: var(--el-color-primary);
+  }
   
+  .el-icon.avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    text-align: center;
+  }
 </style>
