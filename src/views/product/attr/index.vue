c56968c8ed503da136cb5b6246d02c5d1cc4b5d7
shi_guowei <shiguowei333@outlook.com>
2024-12-20 17:42:44 +0800

    属性管理模块静态搭建

--- src/views/product/attr/index.vue
+++ src/views/product/attr/index.vue
@@ -1,7 +1,41 @@
 <template>
-    <div>
-      <h1>属性管理</h1>
-    </div>
+    <el-card>
+      <el-form :inline="true">
+        <el-form-item label="一级分类">
+          <el-select style="width: 200px">
+            <el-option label="北极"></el-option>
+            <el-option label="南极"></el-option>
+            <el-option label="月球"></el-option>
+            <el-option label="火星"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="二级分类">
+          <el-select style="width: 200px">
+            <el-option label="北极"></el-option>
+            <el-option label="南极"></el-option>
+            <el-option label="月球"></el-option>
+            <el-option label="火星"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="三级分类">
+          <el-select style="width: 200px">
+            <el-option label="北极"></el-option>
+            <el-option label="南极"></el-option>
+            <el-option label="月球"></el-option>
+            <el-option label="火星"></el-option>
+          </el-select>
+        </el-form-item>
+      </el-form>
+    </el-card>
+    <el-card style="margin: 10px 0;">
+      <el-button type="primary" icon="Plus">添加属性</el-button>
+      <el-table border style="margin: 10px 0;">
+        <el-table-column label="序号" type="index" align="center" width="80px"></el-table-column>
+        <el-table-column label="属性名称" align="center" width="120px"></el-table-column>
+        <el-table-column label="属性值名称" align="center"></el-table-column>
+        <el-table-column label="操作" align="center" width="120px"></el-table-column>
+      </el-table>
+    </el-card>
 </template>
   
 <script setup lang='ts'>
