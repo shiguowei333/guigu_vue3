3dab6c4971b8403942cbccc0e757d2dd4b5dd993
sakura3 <shiguowei333@outlook.com>
2024-12-25 23:31:27 +0800

    商品详情静态搭建

--- src/views/product/sku/index.vue
+++ src/views/product/sku/index.vue
@@ -15,7 +15,7 @@
               <template #="{row,$index}">
                 <el-button type="primary" :icon="row.isSale==1?'Bottom':'Top'" @click="updateSale(row)"></el-button>
                 <el-button type="primary" icon="Edit" @click="updateSku"></el-button>
-                <el-button type="primary" icon="InfoFilled"></el-button>
+                <el-button type="primary" icon="InfoFilled" @click="findSku"></el-button>
                 <el-button type="primary" icon="Delete"></el-button>
               </template>
             </el-table-column>
@@ -29,6 +29,56 @@
          @change="getHasSku"
         >
       </el-pagination>
+      <el-drawer
+        v-model="drawer"
+        title="查看商品详情"
+      >
+        <template #header>
+          <h4>set title by slot</h4>
+        </template>
+        <template #default>
+          <el-row style="margin: 10px 0;">
+              <el-col :span="6">名称</el-col>
+              <el-col :span="18">华为mata</el-col>
+          </el-row>
+          <el-row style="margin: 10px 0;">
+              <el-col :span="6">描述</el-col>
+              <el-col :span="18">华为mata</el-col>
+          </el-row>
+          <el-row style="margin: 10px 0;">
+              <el-col :span="6">价格</el-col>
+              <el-col :span="18">华为mata</el-col>
+          </el-row>
+          <el-row style="margin: 10px 0;">
+              <el-col :span="6">平台属性</el-col>
+              <el-col :span="18">
+                <el-tag style="margin: 5px;" v-for="item in 10" type="success"></el-tag>
+              </el-col>
+          </el-row>
+          <el-row style="margin: 10px 0;">
+              <el-col :span="6">销售属性</el-col>
+              <el-col :span="18">
+                <el-tag style="margin: 5px;" v-for="item in 10" type="success"></el-tag>
+              </el-col>
+          </el-row>
+          <el-row style="margin: 10px 0;">
+              <el-col :span="6">商品图片</el-col>
+              <el-col :span="18">
+                <el-carousel :interval="4000" type="card" height="200px">
+                  <el-carousel-item v-for="item in 6" :key="item">
+                    <h3 text="2xl" justify="center">{{ item }}</h3>
+                  </el-carousel-item>
+                </el-carousel>
+              </el-col>
+          </el-row>
+        </template>
+        <template #footer>
+          <div style="flex: auto">
+            <el-button @click="cancelClick">cancel</el-button>
+            <el-button type="primary" @click="confirmClick">confirm</el-button>
+          </div>
+        </template>
+      </el-drawer>
     </el-card>
 </template>
   
@@ -41,6 +91,7 @@ import { ElMessage } from 'element-plus'
   let pageSize = ref(10)
   let total = ref(0)
   let skuArr = ref<SkuData[]>()
+  let drawer = ref<boolean>(false)
 
   onMounted(() => {
     getHasSku()
@@ -78,8 +129,26 @@ import { ElMessage } from 'element-plus'
         message: '功能正在开发中~'
       })
   }
+
+  const findSku = () => {
+    drawer.value = true
+  }
 </script>
   
 <style scoped lang="scss">
+  .el-carousel__item h3 {
+    color: #475669;
+    opacity: 0.75;
+    line-height: 200px;
+    margin: 0;
+    text-align: center;
+  }
   
+  .el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+  }
+  
+  .el-carousel__item:nth-child(2n + 1) {
+    background-color: #d3dce6;
+  }
 </style>
