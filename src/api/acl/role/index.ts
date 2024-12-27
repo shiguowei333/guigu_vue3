6e38cc66e5ef1e800e17e257ab842844a9a9b283
shi_guowei <shiguowei333@outlook.com>
2024-12-27 18:30:52 +0800

    添加角色静态的搭建

--- src/api/acl/role/index.ts
+++ src/api/acl/role/index.ts
@@ -1,8 +1,18 @@
 import request from '@/utils/requests'
-import type { RoleResponseData } from './type'
+import type { RoleResponseData, RoleData } from './type'
 
 enum API {
-    ALLROLE_URL = '/admin/acl/role/'
+    ALLROLE_URL = '/admin/acl/role/',
+    ADDROLE_URL = '/admin/acl/role/save',
+    UPDATEROLE_URL = '/admin/acl/role/update'
 }
 
 export const reqAllRoleList = (page: number,limit: number,roleName: string) => request.get<any,RoleResponseData>(API.ALLROLE_URL + `${page}/${limit}?roleName=${roleName}`)
+
+export const reqAddOrUpdateRole = (data: RoleData) => {
+    if(data.id) {
+        return request.put(API.UPDATEROLE_URL,data)
+    }else {
+        return request.post(API.ADDROLE_URL,data)
+    }
+}
--- src/views/acl/role/index.vue
+++ src/views/acl/role/index.vue
@@ -11,7 +11,7 @@
       </el-form>
     </el-card>
     <el-card style="margin: 10px 0;">
-      <el-button type="primary" icon="Plus">添加职位</el-button>
+      <el-button type="primary" icon="Plus" @click="addRole">添加职位</el-button>
       <el-table border style="margin: 10px 0;" :data="allRole">
         <el-table-column type="index" aligin="center" label="#"></el-table-column>
         <el-table-column prop="id" align="center" label="id"></el-table-column>
@@ -21,7 +21,7 @@
         <el-table-column align="center" label="操作" width="400px">
           <template #="{row,$index}">
             <el-button type="primary" icon="User" @click="">分配权限</el-button>
-            <el-button type="primary" icon="Edit" @click="">编辑</el-button>
+            <el-button type="primary" icon="Edit" @click="updateRole(row)">编辑</el-button>
             <el-popconfirm :title="`确定要删除${row.name}吗`" @confirm="">
               <template #reference>
                 <el-button type="primary" icon="Delete">删除</el-button>
@@ -38,18 +38,31 @@
          :total="total"
          @change="getRole"></el-pagination>
     </el-card>
+    <el-dialog v-model="dialogVisite" :title="RoleParms.id?'修改职位':'添加职位'">
+      <el-form>
+        <el-form-item label="职位名称">
+          <el-input placeholder="请输入职位名称"></el-input>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+          <el-button type="primary" @click="dialogVisite=false">取消</el-button>
+          <el-button type="primary" @click="">确定</el-button>
+        </template>
+    </el-dialog>
 </template>
   
 <script setup lang='ts'>
-  import { onMounted, ref } from 'vue'
+  import { onMounted, ref, reactive } from 'vue'
   import { reqAllRoleList } from '@/api/acl/role'
-  import type { RoleResponseData, Records } from '@/api/acl/role/type'
+  import type { RoleResponseData, Records, RoleData } from '@/api/acl/role/type'
 
   let pageNo = ref<number>(1)
   let pageSize = ref<number>(10)
   let total = ref<number>(0)
   let keyWord = ref<string>('')
   let allRole = ref<Records>([])
+  let dialogVisite = ref<boolean>(false)
+  let RoleParms = reactive({})
 
   onMounted(() => {
     getRole()
@@ -70,6 +83,14 @@
   const reset = () => {
     keyWord.value = ''
     getRole()
+  }
+
+  const addRole = () => {
+    dialogVisite.value = true
+  }
+
+  const updateRole = (row: RoleData) => {
+    dialogVisite.value = true
   }
 </script>
   
