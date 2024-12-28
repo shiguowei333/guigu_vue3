b38861cef5b042d650fd72dc95a39a11ffc79966
sakura3 <shiguowei333@outlook.com>
2024-12-28 21:34:45 +0800

    数据大屏框架和顶部导航栏的搭建

--- src/views/screen/index.vue
+++ src/views/screen/index.vue
@@ -1,13 +1,68 @@
 <template>
-    <div>
-      <h1>我是数据大屏一级组件</h1>
+    <div class="container">
+      <div class="screen" ref="screen">
+        <div class="top">
+          <Top></Top>
+        </div>
+        <div class="bottom">
+          <div class="left"></div>
+          <div class="center"></div>
+          <div class="right"></div>
+        </div>
+      </div>
     </div>
 </template>
   
 <script setup lang='ts'>
-  
+  import { ref, onMounted } from 'vue'
+  import Top from './components/top/index.vue'
+
+  let screen = ref()
+
+  onMounted(() => {
+    screen.value.style.transform = `scale(${getScale()}) translate(-50%,-50%)`
+  })
+
+  window.onresize = () => {
+    screen.value.style.transform = `scale(${getScale()}) translate(-50%,-50%)`
+  }
+
+  const getScale = (w =1920, h = 1080) => {
+    const ww = window.innerWidth / w
+    const wh = window.innerHeight / h
+    return ww < wh ?ww:wh
+  }
 </script>
   
 <style scoped lang="scss">
-  
+  .container {
+    width: 100vw;
+    height: 100vh;
+    background: url(./images/bg.png) no-repeat;
+    background-size: cover;
+    .screen {
+      position: fixed;
+      left: 50%;
+      top: 50%;
+      width: 1920px;
+      height: 1080px;
+      transform-origin: left top;
+      .top {
+        width: 100%;
+        height: 40px;
+      }
+      .bottom {
+        display: flex;
+        .left {
+          flex: 1;
+        }
+        .center {
+          flex: 2;
+        }
+        .right {
+          flex: 1;
+        }
+      }
+    }
+  }
 </style>
